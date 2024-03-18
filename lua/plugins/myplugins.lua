bb6b90cd9edeed33076fed03da3c37789f1e42f8
Teepob Harutaipree <Teepob.Harutaipree@agoda.com>
2024-03-18 18:45:04 +0700

    auto: 2024-03-18:18.44

--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -329,95 +329,6 @@ local plugins = {
             },
         }
         opts.defaults = vim.tbl_deep_extend('force', opts.defaults, defaultOverride)
-
-
-      -- local session_picker = function(opts)
-      --   local conf = require("telescope.config").values
-      --   local finders = require "telescope.finders"
-      --   local pickers = require "telescope.pickers"
-      --   local action_state = require "telescope.actions.state"
-      --   local session_dir = vim.g.startify_session_dir or '~/.config/session'
-      --   -- Logic to handle session previews using the session directory
-      --   -- You can customize this to display session information or previews
-      --   -- Example: Display session files in the specified directory
-      --   local results = {}
-      --   for file in io.popen('ls ' .. session_dir):lines() do
-      --       table.insert(results, { value = file })
-      --   end
-      --
-      --   local actions = require('telescope.actions')
-      --
-      --   actions.select_default:replace(function(prompt_bufnr)
-      --     local entry = actions.get_selected_entry(prompt_bufnr)
-      --     if entry then
-      --       vim.cmd('SSave ' .. entry.value)
-      --     end
-      --   end)
-      --
-      --   -- add key map for loadding with SLoad when press C-enter
-      --   -- actions.select_default:replace(function(prompt_bufnr)
-      --   --  local entry = actions.get_selected_entry(prompt_bufnr)
-      --   --  if entry this_offset_encoding
-      --   --
-      --
-      --   -- Return the results for display in Telescope
-      --   return pickers.new(opts, {
-      --     prompt_title = 'Startify Sessions',
-      --   attach_mappings = function(prompt_bufnr, map)
-      --
-      --       -- map("i", "asdf", function(_prompt_bufnr)
-      --       --   print "You typed asdf"
-      --       -- end)
-      -- --
-      --     map('i', '<C-s>', function(_prompt_bufnr)
-      --         print "Saving"
-      --       local entry = action_state.get_selected_entry()
-      --       if entry then
-      --         vim.cmd('SSave ' .. entry.value)
-      --         end
-      --       end)
-      --
-      --     map('i', '<C-CR>', function(_prompt_bufnr)
-      --         print "typed c-enter "
-      --       local entry = action_state.get_selected_entry()
-      --       if entry then
-      --         vim.cmd('SLoad ' .. entry.value)
-      --         end
-      --       end)
-      --
-      --     map('i', '<C-d>', function(_prompt_bufnr)
-      --       local entry = action_state.get_selected_entry()
-      --       print("Deleting" .. entry.value)
-      --
-      --       if entry then
-      --         vim.cmd('SDelete ' .. entry.value)
-      --         end
-      --       end)
-      --
-      --       --- end ---
-      --       return true
-      --
-      --     end,
-      --     finder = finders.new_table {
-      --       results = results,
-      --       entry_maker = function(entry)
-      --         return {
-      --           display = entry.value,
-      --           value = entry.value,
-      --           ordinal = entry.value,
-      --         }
-      --       end,
-      --     },
-      --     sorter = conf.generic_sorter(opts),
-      --   -- })
-      --   }):find()
-      --
-      --   -- return require("telescope").register_extension{
-      --   --   exports = { startify = session_picker }
-      --   -- }
-      -- end
-      --
-      -- map('n', '<leader>fs', session_picker, { desc = "Startify Sessions" })
       map('n', '<leader>fs', function()
         overrides.telescope.session_pickers()
       end
@@ -433,7 +344,7 @@ local plugins = {
       
       -- opts.extensions_list = utils.combineUniqueLists(opts.extensions_list, {'startify'})
 
-        print(vim.inspect(opts))
+        -- print(vim.inspect(opts))
 
         return opts
     end,
