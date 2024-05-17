ffed4da5e5f3b4ab94afc1016a59e56d289d3dad
Teepob Harutaipree <Teepob.Harutaipree@agoda.com>
2024-05-18 02:12:16 +0700

    remove unused utils

--- lua/myutils.lua
+++ lua/myutils.lua
@@ -1,38 +1,5 @@
 local M = {}
 
--- first key duplicate = take the first arg priority "keep"
-
----@class Opts
----@field behavior string The behavior to use when combining dictionaries. Must be "force" or "keep".
-
----@param opts Opts|any
-M.combine_dicts = function(opts, ...)
-	local combined_dict = {}
-	local dicts = { ... }
-	-- if behavior is not set or emptry default = force
-	local behavior = opts.behavior
-
-	if not behavior or behavior == "" then
-		behavior = "force"
-	end
-
-	for _, dict in ipairs(dicts) do
-		combined_dict = vim.tbl_extend(behavior, combined_dict, dict)
-	end
-
-	return combined_dict
-end
-
--- -- Testing
--- sample input output  for combine dicts
--- local a = { a = 1, b = 2 }
--- local b = { c = 3, d = 4 }
--- local c = { a = 5, f = 6 }
---
--- local d = M.listUniqOnly(a, b, c)
--- print(vim.inspect(d))
--- -- { a = 1, b = 2, c = 3, d = 4, e = 5, f = 6 }
-
 M.combineUniqueLists = function(...)
 	local combined = {}
 	local seen = {}
